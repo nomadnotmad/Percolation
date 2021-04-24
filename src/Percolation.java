f2a9bd84da68673b04bdcbd8cff41b67ae967fb3
k-i-a <nomadnotmad>
2021-04-24 15:59:42 +0300

    Исправлена работа програмы, обнаружена ошибка в позиционировании строк и столбцов

--- src/Percolation.java
+++ src/Percolation.java
@@ -2,50 +2,55 @@ public class Percolation {
 
     private int[] id;
     private int[] weight;
-    private boolean[] blocked;
+    private boolean[] unblocked;
     private int N, n, countOpen = 0;
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n) {
-        n = n;
-        N = n * n + 2;
+    public Percolation(int size) {
+        n = size;
+        N = n * n + 3;
         id = new int[N];
         weight = new int[N];
-        blocked = new boolean[N];
+        unblocked = new boolean[N];
         for (int i = 0; i < N; i++) {
             id[i] = i;
-            weight[i] = 0;
-            blocked[i] = true;
+            weight[i] = 1;
+            unblocked[i] = false;
         }
-        blocked[0] = false;
-        blocked[N - 1] = false;
+        unblocked[N - 2] = true;
+        unblocked[N - 1] = true;
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         if (!isOpen(row, col)) {
             countOpen++;
-            blocked[row + (col - 1) * n] = false;
-            if (col == 1) union(row, col, 0, 1);
-            if (col == n) union(row, col, n + 1, n);
+            unblocked[(row - 1) + (col - 1) * n] = true;
+            if (col == 1) union(row, col, n + 2, n);
+            if (col == n) union(row, col, n + 3, n);
             union(row, col, row, col + 1);
             union(row, col, row, col - 1);
             union(row, col, row + 1, col);
             union(row, col, row - 1, col);
         }
+        else System.out.format("\n( %d : %d ) is close \n", row, col);
     }
 
     // is the site (row, col) open?
+    //refreshed
     public boolean isOpen(int row, int col) {
-        if ((col > 0 && col <= n) && (row >= 0 && row <= (n + 1))) {
-            int i = row + (col - 1) * n;
-            return !blocked[i];
+        if ((row > 0 && row <= n) && (col > 0 && col <= n)) {
+            int i = (row - 1) + (col - 1) * n;
+            return unblocked[i];
         }
-        else return false;
+        else if (( (row == n + 2) || (row == n + 3) ) && (col == n)) return true;
+            else {
+                return false;
+        }
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        if (root(0, 1) == root (row, col)) return true;
+        if (root(n + 2, n) == root (row, col)) return true;
         else return false;
     }
 
@@ -56,7 +61,7 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        if (root(0, 1) == root (n + 1, n)) return true;
+        if (root(n + 2, n) == root (n + 3, n)) return true;
         else return false;
     }
 
@@ -73,7 +78,7 @@ public class Percolation {
 
     private int root(int row, int col)
     {
-        int i = row + (col - 1) * n;
+        int i = (row - 1) + (col - 1) * n;
         while (i != id[i]) {
             id[i] = id[id[i]];
             i = id[i];
@@ -82,9 +87,10 @@ public class Percolation {
     }
 
     private void print() {
-        for (int i = 0; i < N; i++) {
-            System.out.print("id: " + id[i] + " we: " + weight[i] + " bl: " + blocked[i] + " ");
-            //if (i % n == 0) System.out.println();
+        for (int i = 0; i < N - 3; i++) {
+            if (i % n == 0) System.out.println();
+            System.out.format("%d ", id[i]);
+
         }
     }
     // test client (optional)
@@ -93,7 +99,9 @@ public class Percolation {
         Percolation percolation = new Percolation(n);
         System.out.println(percolation.numberOfOpenSites());
         System.out.println(percolation.percolates());
+        //System.out.println(percolation.isFull(1,1));
         percolation.open(1,1);
+        //System.out.println(percolation.isFull(1,1));
         System.out.println(percolation.numberOfOpenSites());
         System.out.println(percolation.percolates());
         percolation.open(2,2);
@@ -103,5 +111,6 @@ public class Percolation {
         System.out.println(percolation.numberOfOpenSites());
         System.out.println(percolation.percolates());
         percolation.print();
+        System.out.println(percolation.isFull(2,1));
     }
 }
